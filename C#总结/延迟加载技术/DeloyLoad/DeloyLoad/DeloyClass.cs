93f4eb70dd84ff037781ba76526dffdcdf6c66a7
吴佳泽 <1051277749@qq.com>
2018-05-20 23:52:23 +0800

    2018.5.20

---
+++ C#总结/延迟加载技术/DeloyLoad/DeloyLoad/DeloyClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeloyLoad
+{
+public class DeloyClass
+    {
+        public static string testStr = "333";
+        public static string Str = Show("222");
+        public static string Show(string str)
+        {
+            Console.WriteLine("内部" + str);
+            return str;
+        }
+
+        
+        static DeloyClass()
+        {
+            Console.WriteLine("静态构造");
+        }
+    }
+}
---
+++ C#总结/延迟加载技术/DeloyLoad/DeloyLoad/TestClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeloyLoad
+{
+     public class TestClass
+     {
+         public static string Str = Show("111");
+         public static string Show(string str)
+         {
+             Console.WriteLine("内部"+ str);
+             return str;
+         }
+     }
+}
