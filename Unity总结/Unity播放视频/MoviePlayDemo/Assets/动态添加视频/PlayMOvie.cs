ad99d36a6f0ba89b9571253ed14b11398fd2ecd2
wujiaze <1051277749@qq.com>
2018-05-25 20:19:28 +0800

    2018.5.25

--- Unity总结/Unity播放视频/MoviePlayDemo/Assets/动态添加视频/PlayMOvie.cs
+++ Unity总结/Unity播放视频/MoviePlayDemo/Assets/动态添加视频/PlayMOvie.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Video;
 
+// 优点：可动态设置分辨率
+//      可动态更换视频素材
 public class PlayMOvie : MonoBehaviour {
 
     public VideoPlayer videoPlayer;
@@ -15,21 +17,19 @@ public class PlayMOvie : MonoBehaviour {
         Application.runInBackground = true;
         StartCoroutine(playVideo());
     }
-    
-
     IEnumerator playVideo()
     {
         videoPlayer.playOnAwake = false;
         audioSource.playOnAwake = false;
         videoPlayer.isLooping = true;
         videoPlayer.source = VideoSource.VideoClip;
-        // 设置音频
+        // 设置音频 这种方法设置的是Video自带的音频
         videoPlayer.audioOutputMode = VideoAudioOutputMode.AudioSource;
         videoPlayer.SetTargetAudioSource(0, audioSource); // 为index为0的音轨，设置目标audio
-
-
-        videoPlayer.clip = videoclip;
-        videoPlayer.Prepare();
+        // 这种方法是设置我们添加的音频
+        //audioSource.clip = xxxx;
+        videoPlayer.clip = videoclip; // 这一步可以通过资源加载完成
+        videoPlayer.Prepare(); // 等待视频加载完成
         while (!videoPlayer.isPrepared)
         {
             Debug.Log("Preparing Video");
