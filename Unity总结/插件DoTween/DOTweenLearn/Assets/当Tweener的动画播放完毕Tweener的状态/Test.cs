52dce4761e8328ad48dfb12a4601369b2541c70a
吴佳泽 <1051277749@qq.com>
2018-08-13 23:59:05 +0800

    2018/08/13 周一

---
+++ Unity总结/插件DoTween/DOTweenLearn/Assets/当Tweener的动画播放完毕Tweener的状态/Test.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    private Tweener tweener;
+    void Start()
+    {
+        Debug.Log("isBackwards" + tweener.isBackwards);
+        Debug.Log("IsActive" + tweener.IsActive());
+        Debug.Log("IsComplete" + tweener.IsComplete());
+        Debug.Log("IsInitialized" + tweener.IsInitialized());
+        Debug.Log("IsBackwards" + tweener.IsBackwards());
+        Debug.Log("IsPlaying" + tweener.IsPlaying());
+        tweener.Kill();
+        tweener = transform.DOMove(new Vector3(5, 5, 5), 3);
+        tweener.Kill();
+    }
+
+
+    void Update()
+    {
+        if (tweener != null)
+        {
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                Debug.Log("isBackwards" + tweener.isBackwards);
+                Debug.Log("IsActive" + tweener.IsActive());
+                Debug.Log("IsComplete" + tweener.IsComplete());
+                Debug.Log("IsInitialized" + tweener.IsInitialized());
+                Debug.Log("IsBackwards" + tweener.IsBackwards());
+                Debug.Log("IsPlaying" + tweener.IsPlaying());
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            tweener.Kill();
+        }
+    }
+}
