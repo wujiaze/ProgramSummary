442a8c75760232788b49252ea1611af61b7a3b68
wujiaze <1051277749@qq.com>
2018-12-02 14:23:21 +0800

    2018/12/02 周日

--- C#总结/基本数据类型和数字的转换/NumberConvertBit/NumberConvertBit/Program.cs
+++ C#总结/基本数据类型和数字的转换/NumberConvertBit/NumberConvertBit/Program.cs
@@ -12,18 +12,34 @@
  *      三、工具： byte
  *          因为byte能表示 0000 0000 ~ 1111 1111 ,所以用它可以作为工具
  *
- *      四、原理：
- *          byte数组，随着索引的增加,地址随之增加
+ *      四、小端环境原理：
+ *          byte数组，随着索引的增加,地址随之增加(todo 大端系统测试)
  *          小端系统:低地址存放低位字节，高地址存放高位字节
  *
- *
+ *      五、大小端转换
  *      本地转换 ：X86CPU是小端系统，所以本地的数据保存都遵循 低地址保存低字节 高地址保存高字节
- *      网络转换 ：网络都是大端，所以根据本地的大小端来判断到底需要不需要进行大小端转换
- *          大小端转换:
- *      本地存储于字符串的区别  发送字符串，不需要考虑字节顺序
+ *
+ *      网络转换 ：网络都是大端，所以根据本地的大小端 以及对方的大小端 来判断到底需要不需要进行大小端转换
+ *               对方是小端  网络是大端  己方是小端  -- 刚好"抵消"，不需要大小端转换，但是标准的做法是 双方都做一下转换，这样就可以跨平台一劳永逸
+ *               对方是小端  网络是大端  己方是大端  -- 需要进行大小端转换
+ *               对方是大端  网络是大端  己方是小端  -- 需要进行大小端转换
+ *               对方是大端  网络是大端  己方是大端  -- 不需要进行大小端转换
+ *        总结: 1、对方和己方的大小端相同时，无需对传输的字节数组进行大小端转换 (负数)
+ *              2、对方和己方的大小端不同时，需要由其中一方转换，约定是使数据在网络以大端传输，所以一般是小端系统转换成大端
+ *                 2.1、大小端转换方法:
+ *                          详见 NumberNetConvertTool 
+ *
+ *      六、存储
+ *          不能直接将 数组转换成byte数组，写入文本，还是需要使用字符串，采用字符编码写入文本，或者使用序列化的方法
+ *
+ *      七、字符串为什么不需要大小端转换
+ *          1、各种字符，在所有的编码方式中，都有一套内部的方法(编码方案)，不受外界环境的影响
+ *          2、数字字符串，在大部分编码中都是单字节表示(内部的方法)，只有UTF16是双字节表示，所以UTF16有大小端，但是也不用手动转，提供了方法
+ *          总结：字符串，不需要考虑字节顺序
  */
 
 using System;
+using System.IO;
 
 namespace NumberConvertBit
 {
@@ -32,16 +48,15 @@ namespace NumberConvertBit
         static void Main(string[] args)
         {
             Program program = new Program();
-
             //program.JudgeSystem();
             //program.TestGetBytes();
             //program.TestGetNumber();
-            program.TestTostring();
-            //// 将本地的int数值
-            //int cc = 1;
-            //Console.WriteLine(IPAddress.HostToNetworkOrder(cc));
+            //program.TestTostring();
+            program.NetWorkApply();
             Console.Read();
         }
+
+
 
         /// <summary>
         /// 判断系统的大端小端
@@ -54,6 +69,7 @@ namespace NumberConvertBit
             else
                 Console.WriteLine("大端系统");
         }
+
         /// <summary>
         /// 测试各类数据转换成Byte数组
         /// </summary>
@@ -93,7 +109,8 @@ namespace NumberConvertBit
             }
 
             Console.WriteLine("-------int------");
-            int intMins = -1111111111; // 原码 1100 0010 0011 1010 0011 0101 1100 0111 补码 1011 1101 1100 0101 1100 1010 0011 1001 --> 16进制表示 0xBD C5 CA 39
+            int intMins =
+                -1111111111; // 原码 1100 0010 0011 1010 0011 0101 1100 0111 补码 1011 1101 1100 0101 1100 1010 0011 1001 --> 16进制表示 0xBD C5 CA 39
             byte[] intBytes = BitConverter.GetBytes(intMins);
             for (int i = 0; i < intBytes.Length; i++)
             {
@@ -125,7 +142,8 @@ namespace NumberConvertBit
             }
 
             Console.WriteLine("-------long------");
-            long longMins = -1111111111; // 原码  1000 0000 0000 0000 0000 0000 0000 0000 0100 0010 0011 1010 0011 0101 1100 0111 补码 1111 1111 1111 1111 1111 1111 1111  1111 1011 1101 1100 0101 1100 1010 0011 1001 --> 16进制表示 0xFF FF FF FF BD C5 CA 39
+            long
+                longMins = -1111111111; // 原码  1000 0000 0000 0000 0000 0000 0000 0000 0100 0010 0011 1010 0011 0101 1100 0111 补码 1111 1111 1111 1111 1111 1111 1111  1111 1011 1101 1100 0101 1100 1010 0011 1001 --> 16进制表示 0xFF FF FF FF BD C5 CA 39
             byte[] longBytes = BitConverter.GetBytes(longMins);
             for (int i = 0; i < longBytes.Length; i++)
             {
@@ -158,7 +176,7 @@ namespace NumberConvertBit
 
             Console.WriteLine("-------bool------");
             bool b1 = true; // 由测试可知 true  在内存中的补码为 0000 0001 ,原码 0000 0001
-            bool b2 = false;// 由测试可知 false 在内存中的补码为 0000 0000 ,原码 0000 0000
+            bool b2 = false; // 由测试可知 false 在内存中的补码为 0000 0000 ,原码 0000 0000
             byte[] boolBytes1 = BitConverter.GetBytes(b1);
             byte[] boolBytes2 = BitConverter.GetBytes(b2);
             for (int i = 0; i < boolBytes1.Length; i++)
@@ -195,7 +213,7 @@ namespace NumberConvertBit
 
             Console.WriteLine("-------float 正数------");
             float fplus = 13.0625f; // 0x41510000 来源详见原码、反码、补码
-            float fmins = -13.0625f;// 0xC1510000
+            float fmins = -13.0625f; // 0xC1510000
             byte[] fplusBytes = BitConverter.GetBytes(fplus);
             byte[] fminsBytes = BitConverter.GetBytes(fmins);
             for (int i = 0; i < fplusBytes.Length; i++)
@@ -241,8 +259,9 @@ namespace NumberConvertBit
                     }
                 }
             }
+
             Console.WriteLine("-----double 负数-----");
-            doubleplus = -15.63;// 0xC02F428F5C28F5C3
+            doubleplus = -15.63; // 0xC02F428F5C28F5C3
             doubleplusBytes = BitConverter.GetBytes(doubleplus);
             for (int i = 0; i < doubleplusBytes.Length; i++)
             {
@@ -259,16 +278,18 @@ namespace NumberConvertBit
 
             // 将Double转换成有符号的Int数
             Console.WriteLine("-----DoubleToInt64Bits 正数-----");
-            long longtype = BitConverter.DoubleToInt64Bits(1);// double 1 的二进制表示 0 011 1111 1111 0000000....16进制表示 0x3FF0000...
-                                                              // 将double的码直接作为 int 的补码
-                                                              // 获得 int 的原码 = 0x3FF0000... = (2^62-1)-（2^52-1）=2^62 - 2^52 = 4607182418800017408
+            long longtype =
+                BitConverter.DoubleToInt64Bits(1); // double 1 的二进制表示 0 011 1111 1111 0000000....16进制表示 0x3FF0000...
+            // 将double的码直接作为 int 的补码
+            // 获得 int 的原码 = 0x3FF0000... = (2^62-1)-（2^52-1）=2^62 - 2^52 = 4607182418800017408
             Console.WriteLine("{0:X}", longtype);
             Console.WriteLine(longtype);
 
             Console.WriteLine("-----DoubleToInt64Bits 负数-----");
-            longtype = BitConverter.DoubleToInt64Bits(-1);// double -1 的二进制表示 1 011 1111 1111 0000000....16进制表示 0xBFF0000...
-                                                          // 将double的码直接作为 int 的补码
-                                                          // 获得 int 的原码 =  1100 0000 0001 0000.....   16进制表示 0xC010000...  = - (2^62)+（2^52）=2^62 - 2^52 = -4616189618054758400
+            longtype = BitConverter
+                .DoubleToInt64Bits(-1); // double -1 的二进制表示 1 011 1111 1111 0000000....16进制表示 0xBFF0000...
+            // 将double的码直接作为 int 的补码
+            // 获得 int 的原码 =  1100 0000 0001 0000.....   16进制表示 0xC010000...  = - (2^62)+（2^52）=2^62 - 2^52 = -4616189618054758400
             Console.WriteLine("{0:X}", longtype);
             Console.WriteLine(longtype);
         }
@@ -277,14 +298,14 @@ namespace NumberConvertBit
         private void TestGetNumber()
         {
             // 正数
-            byte[] bytes = new byte[]{1,2,3,4};//二进制表示 00000001 00000010 00000011 00000100 
+            byte[] bytes = new byte[] { 1, 2, 3, 4 }; //二进制表示 00000001 00000010 00000011 00000100 
             //在小端系统中，低字节存储在低地址 高字节存储在高地址
             //1 是 bytes数组的低地址 --> 转换到int数字的低地址中
             //4 是 bytes数组的高地址 --> 转换到int数字的高地址中
             // int 的内存表示就应该是(补码) 00000100000000110000001000000001 =原码 =67305985  16进制表示0x4030201 
-            int value = BitConverter.ToInt32(bytes,0);
-            Console.WriteLine("Value: {0}, 16进制： {1:X}",value,value);
-            bytes = new byte[] { 1, 2, 3, 132 };//二进制表示 00000001 00000010 00000011 10000100 
+            int value = BitConverter.ToInt32(bytes, 0);
+            Console.WriteLine("Value: {0}, 16进制： {1:X}", value, value);
+            bytes = new byte[] { 1, 2, 3, 132 }; //二进制表示 00000001 00000010 00000011 10000100 
             //在小端系统中，低字节存储在低地址 高字节存储在高地址
             //1   是 bytes数组的低地址 --> 转换到int数字的低地址中
             //132 是 bytes数组的高地址 --> 转换到int数字的高地址中
@@ -295,21 +316,40 @@ namespace NumberConvertBit
             // 别的类型也一样
         }
 
+        /// <summary>
+        /// 测试 BitConverter.ToString 方法
+        /// </summary>
         private void TestTostring()
         {
             byte[] bytes = new byte[] { 1, 2, 3, 4 };
-            string str = BitConverter.ToString(bytes);  // 将byte的值，转换成 16进制 0x01 0x02 0x03 0x04 ,并按顺序写出
+            string str = BitConverter.ToString(bytes); // 将byte的值，转换成 16进制 0x01 0x02 0x03 0x04 ,并按顺序写出
             Console.WriteLine(str);
         }
 
-        /// <summary>
-        /// 网络应用测试
-        /// </summary>
         private void NetWorkApply()
         {
-            // 第一种 Array.Resay
-            // 第二种IPAddress.HostToNetworkOrder()
-            // 第三种IPAddress.NetworkToHostOrder()
+            int intplus = 100000;// 原码 0x 00 01 86 A0
+            Console.WriteLine("---发送方---");
+            Console.WriteLine("方法1");
+            byte[] result = NumberNetConvertTool.NetSendConvert1(intplus);
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("{0:X}", result[i]);
+            }
+            Console.WriteLine("方法2");
+            result = NumberNetConvertTool.NetSendConvert2(intplus);
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("{0:X}", result[i]);
+            }
+            Console.WriteLine("---接受方---");
+            Console.WriteLine("方法1");
+            int intResult = NumberNetConvertTool.NetReceiveConvert1(result);
+            Console.WriteLine(intResult);
+            Console.WriteLine("方法2");
+            result = NumberNetConvertTool.NetSendConvert2(intplus);
+            intResult = NumberNetConvertTool.NetReceiveConvert2(result);
+            Console.WriteLine(intResult);
         }
     }
 }
